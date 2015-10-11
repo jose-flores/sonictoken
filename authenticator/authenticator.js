bfd3035d88d9c97013962f945a0d6602321b6c78
Jose Flores <jose@chefreports.com>
2015-10-10 21:07:14 -0400

    Transmitting audio token

--- authenticator/authenticator.js
+++ authenticator/authenticator.js
@@ -1,10 +1,22 @@
 if (Meteor.isClient) {
   // counter starts at 0
+  MessageReceived = new Mongo.Collection(null);
   Session.setDefault('counter', 0);
-
+  // On some other machine:
+  sserver = new SonicServer({freqMin: 440, freqMax: 1760});
+  sserver.on('message', function(message) {
+    // message is '31415'. Do something with it.
+    console.log(message);
+    MessageReceived.insert({message: message});
+  });
+  sserver.start();
+  
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    messages: function() {
+      return MessageReceived.find();
     }
   });
 
--- identityprovider/identity-access.js
+++ identityprovider/identity-access.js
@@ -1,4 +1,6 @@
 if (Meteor.isClient) {
+  var MESSAGE = '314159';
+
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -12,6 +14,8 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+      ssocket = new SonicSocket({freqMin: 440, freqMax: 1760});
+      ssocket.send(MESSAGE);
     }
   });
 }
