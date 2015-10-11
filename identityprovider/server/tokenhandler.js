68cc275381d2767c034effe2d7508277e1a8394d
Jose Flores <jose@chefreports.com>
2015-10-11 00:57:35 -0400

    Testing the totp code

--- identityprovider/server/tokenhandler.js
+++ identityprovider/server/tokenhandler.js
@@ -1,4 +1,5 @@
 var speakeasy = Npm.require('speakeasy');
 
 var key = speakeasy.generate_key({length: 20, google_auth_qr: true});
-console.log(key);
+var tcode = speakeasy.totp({key: key.base32, encoding:'base32'});
+console.log(tcode);
